3fc6befea32edf14c1edac8be0d51c78d0837ebb
Sebastian F <sebpfdev@gmail.com>
2023-12-13 21:44:52 -0500

    Reorganize backend code into src folder

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Start your image with a node base image
+FROM node:20-alpine
+
+# The /app directory should act as the main application directory
+WORKDIR /app
+
+# Copy the app package and package-lock.json file
+COPY package*.json ./
+
+# Install node packages
+RUN npm install
+
+# Copy src and public directories to the working directory of docker image (/app)
+COPY ./src ./src
+# COPY ./public ./public
+
+EXPOSE 3000
+
+# Start application
+CMD ["npm", "run", "start"]
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 // Import modules
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 // Set server port and create express server
 const PORT = process.env.PORT || 3000;
@@ -8,15 +8,15 @@ const app = express();
 
 // Configure express app for data parsing
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 // Routes
-const apiRouter = require("./backend/routes/apiRoutes");
-const htmlRouter = require("./backend/routes/htmlRoutes");
+const apiRouter = require('./backend/routes/apiRoutes');
+const htmlRouter = require('./backend/routes/htmlRoutes');
 
-app.use("/api", apiRouter);
-app.use("/", htmlRouter);
+app.use('/api', apiRouter);
+app.use('/', htmlRouter);
 
 // Initiate server operation
 app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
